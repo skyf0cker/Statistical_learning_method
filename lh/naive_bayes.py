899c83f717d0de67672360b950fdf02d4b0583f8
body class=git-user id=keys-show style=div id=MathJax_Message style=display <vophanlee@gmail.com>
2019-01-29 19:06:02 +0800

    correct a mistake

--- lh/naive_bayes.py
+++ lh/naive_bayes.py
@@ -14,6 +14,8 @@ class NaiveBayes:
         self.data = self.data_make(data_dim, data_num)
         self.mean = np.zeros((class_num, data_dim)).reshape((class_num, data_dim))
         self.var = np.zeros((class_num, data_dim)).reshape((class_num, data_dim))
+        self.p_y1 = 0
+        self.p_y2 = 0
 
     def data_make(self, data_dim, data_num):
         """
@@ -43,6 +45,8 @@ class NaiveBayes:
         """
         data_list_1 = [data["data"] for data in self.data if data["flag"] == 1]
         data_list_2 = [data["data"] for data in self.data if data["flag"] == -1]
+        self.p_y1 = len(data_list_1)/self.data_num
+        self.p_y2 = len(data_list_2)/self.data_num
         self.mean[0] = np.mean(data_list_1, axis=0, keepdims=True).reshape(self.data_dim)
         self.mean[1] = np.mean(data_list_2, axis=0, keepdims=True).reshape(self.data_dim)
         self.var[0] = np.std(data_list_1, axis=0, keepdims=True, ddof=1).reshape(self.data_dim)
@@ -56,6 +60,9 @@ class NaiveBayes:
         """
         p1 = (1/(pow(2*math.pi, 0.5)*self.var[0][0]))*pow(math.e, -(pow((data[0] - self.mean[0][0]), 2)/2*pow(self.var[0][0], 2)))*(1/(pow(2*math.pi, 0.5)*self.var[0][1]))*pow(math.e, -(pow((data[1] - self.mean[0][1]), 2)/2*pow(self.var[0][1], 2)))
         p2 = (1/(pow(2*math.pi, 0.5)*self.var[1][0]))*pow(math.e, -(pow((data[0] - self.mean[1][0]), 2)/2*pow(self.var[1][0], 2)))*(1/(pow(2*math.pi, 0.5)*self.var[1][1]))*pow(math.e, -(pow((data[1] - self.mean[1][1]), 2)/2*pow(self.var[1][1], 2)))
+
+
+        print(p1,',',p2)
 
         if p2 > p1:
             print("[*]: the data belongs to the second class")
