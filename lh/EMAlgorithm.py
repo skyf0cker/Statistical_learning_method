2478d68a0cbdec7130e445083488ac317c4f866e
body class=git-user id=keys-show style=div id=MathJax_Message style=display <vophanlee@gmail.com>
2019-04-02 23:26:19 +0800

    EM

---
+++ lh/EMAlgorithm.py
@@ -0,0 +1,52 @@
+import numpy as np
+import math
+
+class EmAlgorithm:
+    """
+    Implements of EM algorithm
+    """
+    def __init__(self, data, models_num):
+        # self.Responsivity
+        self.data = np.array(data)
+        self.data_num = len(data)
+        self.models_num = models_num
+        self.mu = np.array([[1,2]]).T
+        self.theta = np.array([[1,1]]).T
+        # self.alpha = np.array([1/self.models_num for i in range(self.models_num)]).reshape((1,self.models_num))
+        self.alpha = np.array([0.1, 0.9],dtype=float).reshape(1,2)
+        self.gama = np.zeros((self.models_num, self.data_num))
+        self.max_iters = 500
+
+    def compute_gama(self):
+        
+        self.gauss = self._compute_Guass(self.data, self.mu, self.theta)
+        print(self.gauss,"\n",self.alpha.T)
+        self.gama =  self.gauss*self.alpha.T / np.sum(self.gauss*self.alpha.T,axis=0)
+
+    def _compute_Guass(self, x, mu, theta):
+
+        x = np.tile(x, (self.models_num,1))
+        mu = np.tile(mu, (1,self.data_num))
+        theta = np.tile(theta, (1,self.data_num))
+        return 1/np.sqrt(2*np.pi)*theta*np.exp((x-mu)**2/2*theta**2)
+
+    def update(self):
+
+        self.mu = (np.sum(self.gama*self.data, axis=1) / np.sum(self.gama, axis=1)).T.reshape(self.models_num,1)
+
+        self.theta = (np.sum((((np.tile(self.data, (2,1)).T - self.mu.T)**2)*self.gama.T).T,axis=1) / np.sum(self.gama, axis=1)).T.reshape(self.models_num,1)
+
+        self.alpha = ((np.sum(self.gama, axis=1) / self.data_num).T.reshape(self.models_num,1)).T
+
+    def train(self):
+        
+        for i in range(self.max_iters):
+            print(i)
+            self.compute_gama()
+            self.update()
+
+        print("alpha:", self.alpha)
+        print("mu:", self.mu)
+        print("theta:", self.theta)
+
+
--- lh/tdd.py
+++ lh/tdd.py
@@ -1,5 +1,42 @@
-from adaboost import adaboost
+from EMAlgorithm import EmAlgorithm
+import numpy as np
 
-a = adaboost()
 
-a.load_data()
+def create_data(mu0, sigma0, mu1, sigma1, alpha0, alpha1):
+    '''
+    初始化数据集
+    这里通过服从高斯分布的随机函数来伪造数据集
+    :param mu0: 高斯0的均值
+    :param sigma0: 高斯0的方差
+    :param mu1: 高斯1的均值
+    :param sigma1: 高斯1的方差
+    :param alpha0: 高斯0的系数
+    :param alpha1: 高斯1的系数
+    :return: 混合了两个高斯分布的数据
+    '''
+    #定义数据集长度为1000
+    length = 1000
+
+    #初始化第一个高斯分布，生成数据，数据长度为length * alpha系数，以此来
+    #满足alpha的作用
+    data0 = np.random.normal(mu0, sigma0, int(length * alpha0))
+    #第二个高斯分布的数据
+    data1 = np.random.normal(mu1, sigma1, int(length * alpha1))
+
+    #初始化总数据集
+    #两个高斯分布的数据混合后会放在该数据集中返回
+    dataSet = []
+    #将第一个数据集的内容添加进去
+    dataSet.extend(data0)
+    #添加第二个数据集的数据
+    dataSet.extend(data1)
+
+    #返回伪造好的数据集
+    return dataSet
+
+
+data = create_data(2, 2, 4, 2, 0.6, 0.4)
+e = EmAlgorithm(data, 2)
+e.train()
+# a = e.compute_gama()
+# e.update()
